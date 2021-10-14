50431bcbe40b36cc2e3d5af22f417dbf93ac7392
Gizem Ay <gizem.ay@profe.local>
2021-10-14 07:42:34 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef80edac2aa56599cdf964857581b22e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e5a7cb7e-4287-46c2-99b0-8b9491e63dd2'
+};
